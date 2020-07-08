c6860c950f585af61d636020c487041b6578c1fd
Gabriel Günther Centeno <50345282+gabrielgunthercenteno@users.noreply.github.com>
2020-07-08 11:06:23 -0300

    Add files via upload

---
+++ Complexos.h
@@ -0,0 +1,20 @@
+#ifndef COMPLEXOS_H_INCLUDED
+#define COMPLEXOS_H_INCLUDED
+
+typedef struct complexo cplx;
+cplx* cria_cplx(float r,float i);
+void destroi_cplx(cplx* c);
+int real(cplx* c, float *r);
+int imag(cplx* c, float *i);
+int igual_cplx(cplx* c1, cplx* c2);
+int oposto_cplx(cplx* c, cplx* res);
+int soma_cplx(cplx* c1, cplx* c2, cplx* res);
+int subtracao_cplx(cplx* c1, cplx* c2, cplx* res);
+int produto_cplx(cplx* c1, cplx* c2, cplx* res);
+int modulo_cplx(cplx* c, float* res);
+int inverso_cplx(cplx* c1,cplx* res);
+int divisao_cplx(cplx* c1, cplx* c2, cplx* res);
+int conj_cplx(cplx* c1, cplx* res);
+
+
+#endif // COMPLEXOS_H_INCLUDED
